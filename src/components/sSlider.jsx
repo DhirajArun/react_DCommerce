6c82b9df46e3af3ac7aa541b51fd3eb2b5a4016f
Dhiraj Arun <dhirajarun@DMac.local>
2021-07-12 14:28:15 +0530

    added the slides to fake slides

--- src/components/sSlider.jsx
+++ src/components/sSlider.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { getSlides } from "../services/fakeSliderService";
 import {
   faChevronLeft,
   faChevronRight,
@@ -11,65 +12,8 @@ class SSlider extends Component {
   state = {
     currentFirstSlide: 1,
     currentLastSlide: 3,
-    slides: [
-      {
-        img: "https://i1.wp.com/5ocial.com/wp-content/uploads/2020/12/2315DCF7-E77D-4A91-9217-9B00192BA01E.jpeg?fit=536%2C302&ssl=1",
-        cat: ["Bollywood"],
-        title: "16 Movies and Webseries Coming in December",
-        date: "December 4, 2020",
-      },
-      {
-        img: "https://i1.wp.com/5ocial.com/wp-content/uploads/2020/12/2315DCF7-E77D-4A91-9217-9B00192BA01E.jpeg?fit=536%2C302&ssl=1",
-        cat: ["Bollywood"],
-        title: "16 Movies and Webseries Coming in Decembe",
-        date: "December 4, 2020",
-      },
-      {
-        img: "https://i1.wp.com/5ocial.com/wp-content/uploads/2020/12/2315DCF7-E77D-4A91-9217-9B00192BA01E.jpeg?fit=536%2C302&ssl=1",
-        cat: ["Bollywood"],
-        title: "16 Movies and Webseries Coming in Deceber",
-        date: "December 4, 2020",
-      },
-      {
-        img: "https://i1.wp.com/5ocial.com/wp-content/uploads/2020/12/2315DCF7-E77D-4A91-9217-9B00192BA01E.jpeg?fit=536%2C302&ssl=1",
-        cat: ["Bollywood"],
-        title: "16 Movies and Webseries Coming n December",
-        date: "December 4, 2020",
-      },
-      {
-        img: "https://i1.wp.com/5ocial.com/wp-content/uploads/2020/12/2315DCF7-E77D-4A91-9217-9B00192BA01E.jpeg?fit=536%2C302&ssl=1",
-        cat: ["Bollywood", "webseries"],
-        title: "16 Movies and Webseries Coing in December",
-        date: "December 4, 2020",
-      },
-      {
-        img: "https://i1.wp.com/5ocial.com/wp-content/uploads/2020/12/2315DCF7-E77D-4A91-9217-9B00192BA01E.jpeg?fit=536%2C302&ssl=1",
-        cat: ["Bollywood", "Hollywood"],
-        title: "16 Movies and Webseies Coming in December",
-        date: "December 4, 2020",
-      },
-      {
-        img: "https://i1.wp.com/5ocial.com/wp-content/uploads/2020/12/2315DCF7-E77D-4A91-9217-9B00192BA01E.jpeg?fit=536%2C302&ssl=1",
-        cat: ["Bollywood", "Hollywood"],
-        title: "16 Movies and Weseries Coming in December",
-        date: "December 4, 2020",
-      },
-      {
-        img: "https://i1.wp.com/5ocial.com/wp-content/uploads/2020/12/2315DCF7-E77D-4A91-9217-9B00192BA01E.jpeg?fit=536%2C302&ssl=1",
-        cat: ["Bollywood", "Hollywood"],
-        title: "16 Moes and Webseries Coming in December",
-        date: "December 4, 2020",
-      },
-      {
-        img: "https://i1.wp.com/5ocial.com/wp-content/uploads/2020/12/2315DCF7-E77D-4A91-9217-9B00192BA01E.jpeg?fit=536%2C302&ssl=1",
-        cat: ["Bollywood", "Hollywood"],
-        title: "16 Movies and bseries Coming in December",
-        date: "December 4, 2020",
-      },
-    ],
+    slides: getSlides(),
   };
-
-  slideStyles = {};
 
   handleSlideHover(slide, value) {
     const slides = [...this.state.slides];
---
+++ src/services/fakeSliderService.js
@@ -0,0 +1,60 @@
+const slides = [
+    {
+      img: "https://i1.wp.com/5ocial.com/wp-content/uploads/2020/12/2315DCF7-E77D-4A91-9217-9B00192BA01E.jpeg?fit=536%2C302&ssl=1",
+      cat: ["Bollywood"],
+      title: "16 Movies and Webseries Coming in December",
+      date: "December 4, 2020",
+    },
+    {
+      img: "https://i1.wp.com/5ocial.com/wp-content/uploads/2020/12/2315DCF7-E77D-4A91-9217-9B00192BA01E.jpeg?fit=536%2C302&ssl=1",
+      cat: ["Bollywood"],
+      title: "16 Movies and Webseries Coming in Decembe",
+      date: "December 4, 2020",
+    },
+    {
+      img: "https://i1.wp.com/5ocial.com/wp-content/uploads/2020/12/2315DCF7-E77D-4A91-9217-9B00192BA01E.jpeg?fit=536%2C302&ssl=1",
+      cat: ["Bollywood"],
+      title: "16 Movies and Webseries Coming in Deceber",
+      date: "December 4, 2020",
+    },
+    {
+      img: "https://i1.wp.com/5ocial.com/wp-content/uploads/2020/12/2315DCF7-E77D-4A91-9217-9B00192BA01E.jpeg?fit=536%2C302&ssl=1",
+      cat: ["Bollywood"],
+      title: "16 Movies and Webseries Coming n December",
+      date: "December 4, 2020",
+    },
+    {
+      img: "https://i1.wp.com/5ocial.com/wp-content/uploads/2020/12/2315DCF7-E77D-4A91-9217-9B00192BA01E.jpeg?fit=536%2C302&ssl=1",
+      cat: ["Bollywood", "webseries"],
+      title: "16 Movies and Webseries Coing in December",
+      date: "December 4, 2020",
+    },
+    {
+      img: "https://i1.wp.com/5ocial.com/wp-content/uploads/2020/12/2315DCF7-E77D-4A91-9217-9B00192BA01E.jpeg?fit=536%2C302&ssl=1",
+      cat: ["Bollywood", "Hollywood"],
+      title: "16 Movies and Webseies Coming in December",
+      date: "December 4, 2020",
+    },
+    {
+      img: "https://i1.wp.com/5ocial.com/wp-content/uploads/2020/12/2315DCF7-E77D-4A91-9217-9B00192BA01E.jpeg?fit=536%2C302&ssl=1",
+      cat: ["Bollywood", "Hollywood"],
+      title: "16 Movies and Weseries Coming in December",
+      date: "December 4, 2020",
+    },
+    {
+      img: "https://i1.wp.com/5ocial.com/wp-content/uploads/2020/12/2315DCF7-E77D-4A91-9217-9B00192BA01E.jpeg?fit=536%2C302&ssl=1",
+      cat: ["Bollywood", "Hollywood"],
+      title: "16 Moes and Webseries Coming in December",
+      date: "December 4, 2020",
+    },
+    {
+      img: "https://i1.wp.com/5ocial.com/wp-content/uploads/2020/12/2315DCF7-E77D-4A91-9217-9B00192BA01E.jpeg?fit=536%2C302&ssl=1",
+      cat: ["Bollywood", "Hollywood"],
+      title: "16 Movies and bseries Coming in December",
+      date: "December 4, 2020",
+    },
+  ]
+
+  export function getSlides() {
+    return slides
+  }
